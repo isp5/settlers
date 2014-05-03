327c5b25fdb86be3046306a6fa21b4ce19198d08
Ian <isp5@cornell.edu>
2014-05-03 13:34:47 -0400

    wrote the conversion functions game -> state and vice versa along with helper functions to make them simpler

--- release/game.ml
+++ release/game.ml
@@ -7,7 +7,7 @@ open Print
 type playerinfo = player*structures
 
 (*Information for each player, then the rest of the board in structures then turn and next like state *)
-type game = playerinfo*playerinfo*playerinfo*playerinfo*structures*turn*next
+type game = playerinfo*playerinfo*playerinfo*playerinfo*board*turn*next
 
 let get_player_color pi = 
   match (fst pi) with 
@@ -26,8 +26,48 @@ let hide_hand pi =
   | c, (inv, cs) , t -> (c, (inv, hide cs), t), snd pi
 
 
-let state_of_game g = failwith "A voice said look me in the stars"
-let game_of_state s = failwith "And tell me truly, men of earth,"
+let state_of_game g = 
+  match g with 
+  | p1, p2, p3, p4, b, t, n -> b, [(fst p1);(fst p2);(fst p3);(fst p4)], t, n
+
+let rec get_intersections color ls acc = 
+  match ls with 
+  | [] -> acc
+  | hd::tl -> 
+    match hd with 
+    | Some (c, s) -> 
+      if c = color then get_intersections c tl (hd::acc)
+      else get_intersections c tl acc
+    | None -> get_intersections c tl acc
+    
+let rec get_roads color ls acc = 
+  match ls with 
+  | [] -> acc
+  | hd::tl -> 
+    match hd with 
+    | c, l -> 
+      if c = color then get_roads c tl (hd::acc)
+      else get_roads c tl acc
+
+
+let associate_with_player p s : structures = 
+  match p with 
+  | c, _, _ -> 
+    match s with 
+    | inters, roads -> (get_intersections c inters [] , get_roads c roads []) 
+
+let game_of_state s = 
+  match s with 
+  | b, p1::p2::p3::[p4] , t, n -> 
+    match b with 
+    | _, structures , _, _, _ -> 
+      let p1_info = p1, (associate_with_player p1 structures) in
+      let p2_info = p2, (associate_with_player p2 structures) in
+      let p3_info = p3, (associate_with_player p3 structures) in
+      let p4_info = p4, (associate_with_player p4 structures) in
+      p1_info, p2_info, p3_info, p4_info, b, t, n
+      
+    
 
 
 let init_game () = game_of_state (gen_initial_state())
